c9df591ef0363e76686aa623883c2229f072ed39
IordacheRobert <iordacherobertnicolae@gmail.com>
2016-03-25 10:01:02 +0200

    second Commit from my laptop

---
+++ UploadImage/src/main/java/ro/robert/RESTfull/UploadImage/MyResource.java
@@ -0,0 +1,57 @@
+package ro.robert.RESTfull.UploadImage;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+
+@Path("res")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    @GET
+    @Path("/images/{name}")
+    @Produces("image/jpg")
+    public Response downloadImageFile(@PathParam("name")String denumire) {
+    	ClassLoader classLoader = getClass().getClassLoader();
+        // set file (and path) to be download
+    	String path=null;
+    	try{
+    		 path=classLoader.getResource("im ages/"+denumire).getFile();
+    		 path=URLDecoder.decode(path,"UTF-8");
+    	}catch(NullPointerException ex){
+    		System.out.println(ex.getMessage());
+    		 return Response.ok().status(404).build();
+    	} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	System.out.println(path);
+        File file = new File(path);
+ 
+        ResponseBuilder responseBuilder = Response.ok((Object) file);
+       // responseBuilder.header("Content-Disposition", "attachment; filename=\"MyPngImageFile.png\"");
+        return responseBuilder.build();
+    }
+    
+    
+}
